498fa1f6457b1e49c73d8999df9d87106c17fd8f
Robert Kiessling <robert.kiessling.jr@gmail.com>
2024-09-23 13:44:08 -1000

    force tooltips to be outside of left column

--- src/components/ui/tooltip.jsx
+++ src/components/ui/tooltip.jsx
@@ -8,28 +8,29 @@ export default function Tooltip(props) {
     React.useEffect(() => {
         setDomReady(true)
     }, [])
-
     if (!domReady) {
         return null;
     }
 
+    // const place = props.place || 'right';
+    const place = 'right';
+
     // TODO this doesn't work well if the tooltip is forced to move, since it is still offset according to original direction
-    const place = props.place || 'right';
     let offset = {};
-    switch(place) {
-        case 'top':
-            offset = { top: 10 }
-            break;
-        case 'right':
-            offset = { right: 10 }
-            break;
-        case 'bottom':
-            offset = { bottom: 10 }
-            break;
-        case 'left':
-            offset = { left: 10 }
-            break;
-    }
+    // switch(place) {
+    //     case 'top':
+    //         offset = { top: 10 }
+    //         break;
+    //     case 'right':
+    //         offset = { right: 10 }
+    //         break;
+    //     case 'bottom':
+    //         offset = { bottom: 10 }
+    //         break;
+    //     case 'left':
+    //         offset = { left: 10 }
+    //         break;
+    // }
 
     // Need to use createPortal to attach tooltip to an outer element (near <body>) otherwise the
     // tooltip might get clipped by overflow:hidden containers
